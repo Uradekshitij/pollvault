42ba47b57dabe8c4917637e5aeef11d8858b40ff
Kshitij Urade <kshitijurade33@gmail.com>
2026-02-15 16:39:45 +0530

    Added the dist folder

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import pollsRouter from "./routes/polls.js";
 import votesRouter from "./routes/votes.js";
 
+import path from "path";
+
 dotenv.config();
 
 const app = express();
@@ -16,6 +18,14 @@ app.use("/api/polls", pollsRouter);
 app.use("/api/votes", votesRouter);
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV==="production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 mongoose
   .connect(process.env.MONGO_URI)
